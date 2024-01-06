f212d558b37ddaf17d58c20834e7f53b45aea9da
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-01-05 21:42:03 -0400

    Floyd

---
+++ graph_theory/floyd.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define INF 1e18
+#define vi vector<int>
+signed main() {
+    int n; 
+    vector<vi> dp(n, vi(n, INF));
+    // dp[i][i] = 0, for all i-> 0..n-1
+    for (int k = 0; k<n; k++) 
+    for (int i = 0; i<n; i++) 
+    for (int j = 0;j <n; j++) 
+        if (max(dp[i][k], dp[k][j])<INF)
+            dp[i][j] = min(min(dp[i][j], dp[j][i]), dp[i][k]+dp[k][j]);
+
+    return 0;
+}
