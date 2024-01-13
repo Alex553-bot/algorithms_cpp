b09eb4c3725f53217b348289997735a1b1d3d1a9
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-01-13 14:13:40 -0400

    Bellman-Ford  Algorithm

---
+++ graph_theory/bellman.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define int long long 
+#define N 5e3
+#define INF 1e18
+#define F first 
+#define S second
+vector<int> grafo[N+1], v(N+1, 0), dis(N+1, INF);
+signed main() {
+    // realiza el grafo
+    for (int k = 0; k<n; k++) {
+        int modif = 0;
+        for (int i = 1; i<=n; i++) 
+        for (auto it: grafo[i]) {
+            int to = it.F, len = it.S; 
+            if (dis[to]>dis[i]+len) {
+                v[to] = i;
+                dis[to]  = dis[i]+len;
+                modif = 1;
+            }
+        }
+        if (!modif) break;
+    }
+    int ncycle = 0;
+    for (int i = 1; i<=n; i++) 
+        for (auto it: grafo[i]) {
+            int to = it.F, len = it.S; 
+            if (dis[to]>dis[i]+len) {
+                ncycle = i;
+            }
+        }
+
+    if (!ncycle) {
+        // there is no negative cycle
+    } else {
+        // negative cycle 
+    }
+    return 0;
+}
