b57e947fa609da96e516cde7e508f3ff5de074fc
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-01-14 20:49:17 -0400

    lca + bianry lifting over trees

--- graph_theory/bellman.cpp
+++ graph_theory/bellman.cpp
@@ -8,7 +8,9 @@ using namespace std;
 vector<int> grafo[N+1], v(N+1, 0), dis(N+1, INF);
 signed main() {
     // realiza el grafo
-    for (int k = 0; k<n; k++) {
+    // para grafo con multiples destinos
+    // si es para un unico origen solo aumentar: dis[i]!=INF antes de visitar las aristas del nodo.
+	for (int k = 0; k<n; k++) {
         int modif = 0;
         for (int i = 1; i<=n; i++) 
         for (auto it: grafo[i]) {
