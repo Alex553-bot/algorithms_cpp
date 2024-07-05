d5f5664cc4a801e23799b6cc6e253dbad821393a
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-07-05 11:21:03 -0400

    rabin karp + kmp tested

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -2,17 +2,26 @@
 using namespace std;
 #define int long long
 #define vi vector<int> 
-vi kmp(string &s) { // t+'%'+s
-    int n = s.size();
-	vi v(n);
-	for (int i = 1; i<n; i++) {
-		int j = v[i-1];
-		while (j && s[i]!=s[j]) j = v[j-1];
-		if (s[i]==s[j]) j++;
-		v[i] = j;
-	}
-	return v;
-} 
+vi kmpp(string &s, int n) {
+    vi dp(n, 0);
+    int j= 0; 
+    for (int i = 1; i<n; i++) {
+        while (j && s[i]!=s[j]) j = dp[j-1]; 
+        if (s[i]==s[j]) dp[i] = ++j; 
+        else dp[i] = 0;
+    }
+    return dp; 
+}
+vi kmp(string &t, string &p) {
+    string q = p+"#"+t; 
+    vi v = kmpp(q, q.size()); 
+    vi res;
+    for (int i=0 ; i<q.size(); i++) 
+    if (v[i]==p.size()) {
+        res.push_back(i-2*p.size()+1);
+    }
+    return res;
+}
 signed main() {
     return 0;
 }
