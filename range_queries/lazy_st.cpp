bfa69299e53091e8ecd4f4bfbae00d01d5f28ddd
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-08-08 12:03:03 -0400

    lazy + hashing

--- range_queries/lazy_st.cpp
+++ range_queries/lazy_st.cpp
@@ -1,50 +1,49 @@
-#include <bits/stdc++.h>
-using namespace std; 
-#define vi vector<int>
 const int N = 1e5+1;
 const int value = 1e17; 
-vi t(4*N, value), d(4*N, 0);
+vi d(4*N), t(4*N);
 int n, k; 
-int combine(int left, int right) {
-    return min(left, right); 
+int combine(int a, int b) {
+    return a+b;
 }
-void apply(int &f, int &g) {
-    f += g;
+void apply(int &a, int b, int len, int piv) {
+    a+=b*len+piv*(len-1)*len/2;
 }
-void propagate(int i) {
-    if (i<<1>=2*k+n) return;
-    if (i<k) apply(t[i], d[i]);
-    if (i<<1>=k) apply(t[i<<1], d[i]), apply(t[i<<1|1], d[i]);
-    else apply(d[i<<1], d[i]), apply(d[i<<1|1], d[i]);
-    d[i] = 0;
+void propagate(int i, int len) {
+    if (!d[i] || i>=2*k+n) return;
+    apply(t[i], d[i], len, f[i]);
+    if ((i<<1)<2*k+n) {
+        d[i<<1]+=d[i], d[i<<1|1]+=d[i]+f[i]*len/2;
+        f[i<<1]+=f[i], f[i<<1|1]+=f[i];
+    }
+    d[i] = f[i] = 0;
 }
 void upd(int i, int lc, int rc, int l, int r, int x) {
-    if (d[i]) propagate(i);
+    if (l==r) return;
+    propagate(i, rc-lc);
     if (lc==l&&rc==r) {
-        if (i>=k) apply(t[i], x), t[i>>1] = combine(t[i], t[i^1]);
-        else apply(d[i], x);
-    }
-    else {
-        if (i>=k) return;
+        d[i]+=x;f[i]++; 
+        propagate(i, rc-lc);
+    } else {
+        assert(lc!=rc-1);
         int mid = (lc+rc)>>1;
         if (l<mid) upd(i<<1, lc, mid, l, min(r, mid), x);
-        if (r>=mid) upd(i<<1|1, mid, rc, max(mid, l), r, x);
-        propagate(i<<1), propagate(i<<1|1); 
+        if (r>=mid) upd(i<<1|1, mid, rc, max(mid, l), r, x+mid-lc);
+        propagate(i<<1, mid), propagate(i<<1|1, mid);
         t[i] = combine(t[i<<1], t[i<<1|1]);
     }
 }
 int query(int i, int lc, int rc, int l, int r) {
-    if (d[i]) propagate(i);
+    if (l==r) return 0;
+    propagate(i, rc-lc);
     if (lc==l&&rc==r) return t[i];
     else {
-        if (i>=k) return value;
-        int mid = (lc+rc)>>1;
-        int res = value;
+        assert(lc!=rc-1);
+        int mid = (lc+rc)>>1, res =0 ;
         if (l<mid) res = combine(res, query(i<<1, lc, mid, l, min(mid, r)));
         if (r>=mid) res = combine(query(i<<1|1, mid, rc, max(l, mid), r), res);
-	propagate(i<<1), propagate(i<<1|1); 
-	t[i] = combine(t[i<<1], t[i<<1|1]); 
-	return res;
+        propagate(i<<1, mid-lc), propagate(i<<1|1, mid-lc);
+        t[i] = combine(t[i<<1], t[i<<1|1]); 
+        return res;
     }
 }
 void build() {
@@ -57,6 +56,6 @@ signed main() {
     while (k<=n) k<<=1;
     build();
     // upd o query : 
-    // upd(1, 0, k, l, r, value);
+    // f(1, 0, k, l, r, value);
     return 0;
 }
--- strings/hashing.cpp
+++ strings/hashing.cpp
@@ -1,18 +1,42 @@
-#include <bits/stdc++.h> 
-using namespace std;
-typedef long long int ll;
-ll m = 1e9+7;
-ll hashing(string s, ll p) {
-	ll a = 1, res =0 ;
-	for (ll i= 0; i<s.size())
-		res += s[i]*a%m, a*=p, a%=m;
-	return res;
+#include <bits/stdc++.h>
+using namespace std; 
+#define vi vector<int>
+const int MOD = 1e9+7; 
+const int N = 1e6;
+const int primes[3] = {127, 229, 229};
+const int mod[3] = {MOD, 998244353, (1<<31)-1};
+vi pows[3], invs[3];
+vi v[3], rv[3]; 
+int n, m;
+int binpow(int b, int e, int m) {
+    int res = 1; 
+    for(; e; e>>=1, b = (b*b)%m)
+    if (e&1) res = (res*b)%m;
+    return res;
 }
-int main() {
-	// p must be a prime number: 31 is a good choice
-	cin>>m;
-	string s; cin>>s;
-	ll hash = hashing(s, m);
-	cout<<hash<<endl;
-	return 0;
+int inv(int b, int m) {
+    return binpow(b, m-2,m);
+}
+void init() {
+    for (int i=0; i<2; i++) {
+        v[i].resize(N, 0), rv[i].resize(N, 0);
+        pows[i].resize(N, 1), invs[i].resize(N, 1);
+        for (int j= 1; j<N;j ++) {
+            pows[i][j] = (pows[i][j-1]*primes[i])%mod[i];
+            if (j==1)
+                invs[i][j] = inv(pows[i][j], mod[i]);
+            else 
+                invs[i][j] = (invs[i][j-1]*invs[i][1])%mod[i];
+        }
+    }
+}
+void build(string &s, vi *v) {
+    int n = s.size();
+    for (int i = 0; i<2; i++) v[i][0] = s[0];
+    for (int j = 0; j<2;j ++) 
+    for (int i= 1; i<n; i++) 
+        v[j][i] = (v[j][i-1]+(pows[0][i]*s[i])%mod[j])%mod[j];
+}
+int fh(int i, int j, vi &v, int k) {
+    return (((v[j]-(i? v[i-1]: 0)+mod[k])%mod[k])*invs[k][i])%mod[k];
 }
