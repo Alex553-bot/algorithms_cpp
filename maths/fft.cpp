282c07d4d93b39788698225bedbd0b6348c67c80
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-09-10 20:02:51 -0400

    fix; fft

--- maths/fft.cpp
+++ maths/fft.cpp
@@ -1,33 +1,43 @@
-double pi = acos(-1);
-void fft(vi &v, int rev) {
-	int n = v.size(); 
-	if (n==1) return;
-	vector<cd> a(n/2), b(n/2); 
-	for (int i=0 ; i<n/2; i++) 
-		a[i] = v[2*i], b[i] = v[2*i+1]; 
-	double ang = 2*pi/n*(rev?-1:1); 
-	cd w(1), wn(cos(ang), sin(ang)); 
-	for (int k =0 ; k<n; k++) {
-		a[k] = a[k]+w*b[k]; 
-		a[k+n/2] = a[k] - w*b[k]; 
-		if (inv) a[k] /=2, a[k+n/2]/=2; 
-		w*=wn; 
-	}
+using cd = complex<double>;
+const double PI = acos(-1);
+/*only works for n <= 2e5 aprox -> 1(s) and n<=1e6 takes 1.5(s) */
+void fft(vector<cd> & a, int inv = 0) {
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+        if (i < j) swap(a[i], a[j]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (inv ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (inv) 
+        for (cd & x : a) x /= n;
 }
-int main() {
-	int n, m; cin>>n>>m; 
-	vi a(n), b(m); 
-	for (int i =0 ; i<n; i++) cin>>a[i]; 
-	for (int i =0 ; i<m; i++) cin>>b[i];
-	n = 1; 
-	while (a.size()+b.size()>n) n<<=1; 
-	a.resize(n), b.resize(n); 
-	fft(a, 0);
-       	fft(b, 0);	
-	for (int i=0 ; i<n; i++) a[i]*=b[i]; 
-	fft(a, 1); 
-	for (int i = 2*n-1; i>-1; i--) 
-		cout<<a[i]<<" \n"[i==0];
-	
-	return 0;
+vi multiply(vi &a, vi &b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end()); 
+    int n = 1; 
+    while (n<a.size()+b.size()) n<<=1;
+    fa.resize(n), fb.resize(n); 
+    fft(fa), fft(fb); 
+    for (int i =0; i<n;i ++) fa[i]*=fb[i]; 
+    fft(fa, 1); 
+    vi res(n); 
+    for (int i= 0; i<n;i ++) res[i] = round(fa[i].real()); 
+    return res;
 }
