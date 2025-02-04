907f681f4b9caefce6c396b5821543220e9322c5
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2025-02-03 21:40:40 -0400

    hashing

--- strings/hashing.cpp
+++ strings/hashing.cpp
@@ -1,37 +1,35 @@
-const int primes[3] = {127, 229, 229};
-const int mod[3] = {MOD, 998244353, (1<<31)-1};
-vi pows[3], invs[3];
-vi v[3], rv[3]; 
+const int primes[2] = {127, 229};
+const int mods[2] = {1000000007, (1ll<<31)-1};
+vi pows[2], invs[2];
 int n, m;
 int binpow(int b, int e, int m) {
     int res = 1; 
-    for(; e; e>>=1, b = (b*b)%m)
-    if (e&1) res = (res*b)%m;
+    for(; e; e>>=1, b = b*b%m)
+    if (e&1) res = res*b%m;
     return res;
 }
-int inv(int b, int m) {
-    return binpow(b, m-2,m);
-}
+int inv(int b, int m) {return binpow(b, m-2, m);}
 void init() {
     for (int i=0; i<2; i++) {
-        v[i].resize(N, 0), rv[i].resize(N, 0);
         pows[i].resize(N, 1), invs[i].resize(N, 1);
         for (int j= 1; j<N;j ++) {
-            pows[i][j] = (pows[i][j-1]*primes[i])%mod[i];
-            if (j==1)
-                invs[i][j] = inv(pows[i][j], mod[i]);
-            else 
-                invs[i][j] = (invs[i][j-1]*invs[i][1])%mod[i];
+            pows[i][j] = (pows[i][j-1]*primes[i])%mods[i];
+            if (j==1) invs[i][j] = inv(pows[i][j], mods[i]);
+            else invs[i][j] = (invs[i][j-1]*invs[i][1])%mods[i];
         }
     }
 }
-void build(string &s, vi *v) {
-    int n = s.size();
-    for (int i = 0; i<2; i++) v[i][0] = s[0];
-    for (int j = 0; j<2;j ++) 
-    for (int i= 1; i<n; i++) 
-        v[j][i] = (v[j][i-1]+(pows[0][i]*s[i])%mod[j])%mod[j];
-}
-int fh(int i, int j, vi &v, int k) {
-    return (((v[j]-(i? v[i-1]: 0)+mod[k])%mod[k])*invs[k][i])%mod[k];
-}
+struct Hash{ // USO: init() [1 vez] y luego usar normal
+    int n; vi v[2];
+    Hash(string &s): n(s.size()) {
+        for (int i = 0; i<2; i++) v[i].resize(n);
+        for (int i = 0; i<2;i ++) 
+        for (int j = 0; j<n; j++) v[i][j] = (s[j] * pows[i][j]%mods[i] + (j? v[i][j-1]: 0))%mods[i];
+    }
+    int fh(int i, int j, int k) {
+        return (v[k][j]-(i? v[k][i-1]: 0)+mods[k])%mods[k] * invs[k][i] % mods[k];
+    }
+    int get(int i, int j) { // get [i, j]
+        return fh(i, j, 0) + fh(i, j, 1) * 1000000000ll;
+    }
+};
