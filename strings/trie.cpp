ecbc5689ab88ee21e2a21856ed56177ac83a729b
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2025-04-28 20:03:42 -0400

    fix: datastructures

--- strings/trie.cpp
+++ strings/trie.cpp
@@ -1,31 +1,62 @@
 const int MAXSIZE = 26;
-struct trie {
-	struct trie *child[MAXSIZE];
-	bool end; 
+struct node {
+	int end;
+	int grafo[MAXSIZE]
+	node() {
+		for (int i =0 ; i<MAXSIZE; i++) grafo[i] = 0;
+	}
 };
-struct trie *getNode() {
-	struct trie *node = new trie();
-	node->end = false;
-	for (int i= 0; i<MAXSIZE; i++) 
-		node->child[i] = NULL;
-	return node;
-}
-int search(struct trie *root, string &s) {
-	struct trie *proot = root;
-	for (int i= 0; i<s.size(); i++) {
-		int ind = s[i]-'a';
-		if (!proot->child[ind]) return 0;
-		proot = proot->child[ind];
+vector<node> v(1);
+node ini;
+void insert(string &s, int i = 0, int j = 0) {
+	if (j>size(s)) {
+		v[i].end = 1;
+		return;
 	}
-	return proot->end;
+	int to = s[j]-'a';
+	if (!v[i].grafo[to]) v[i].grafo[to] = v.size(), v.pb(ini);
+	insert(s, v[i].grafo[to], j+1);
 }
-void insert(struct trie *root, string &s) {
-	struct trie *proot = root;
-	for (int i= 0; i<s.size(); i++) {
-		int ind = s[i]-'a';
-		if (!proot->child[ind])
-			proot->child[ind] = getNode();
-		proot = proot->child[ind];
-	}
-	proot->end = true;
+void erase(string &s, int i = 0, int j = 0) {
+	if (j>size(s)) {v[i].end = 0; return;}
+	int to = s[j]-'a';
+	if (!v[i].grafo[to]) return;
+	return erase(s, v[i].grafo[to], j+1);
+}
+int get(string &s, int i = 0, int j = 0) {
+	if (j>size(s)) return v[i].end;
+	int to = s[j]-'a';
+	if (v[i].grafo[to]) return get(s, v[i].grafo[to], j+1);
+}
+
+/*FOR BITS:*/
+const int MAXSIZE = 2;
+struct node {
+    int c;
+    int grafo[MAXSIZE];
+    node() {grafo[0] = grafo[1] = 0;}
+};
+vector<node> v(1);
+node ini;
+void insert(int x, int i = 0, int j = 30) {
+    v[i].c++;
+    if (j==-1) return;
+    int to = (x>>j)&1;
+    if (!v[i].grafo[to]) 
+        v[i].grafo[to] = v.size(), v.pb(ini);
+    insert(x, v[i].grafo[to], j-1);
+}
+void erase(int x, int i = 0, int j = 30) {
+    v[i].c--;
+    if (j==-1) return;
+    int to = (x>>j)&1;
+    erase(x, v[i].grafo[to], j-1);
+}
+int get(int x, int i = 0, int j = 30) {
+	// modify this function based on the problem.
+    if (j==-1) return 0;
+    int to = 1^((x>>j)&1);
+    if (!v[i].grafo[to] || !v[v[i].grafo[to]].c) to ^= 1;
+    if (!v[i].grafo[to] || !v[v[i].grafo[to]].c) return 0;
+    return (to<<j) + get(x, v[i].grafo[to], j-1);
 }
