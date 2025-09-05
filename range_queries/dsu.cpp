62b0945bea82d2679c44bdd0cd0ca041af0a3d9a
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2025-09-04 21:25:19 -0400

    add: dsu trick

--- range_queries/dsu.cpp
+++ range_queries/dsu.cpp
@@ -1,16 +1,13 @@
 struct DSU {
-    vi p, rn;
-    int n;
-    DSU(int N): n(N+1) {
-        p.resize(n+1), rn.resize(n+1, 1);
-        for (int i = 0; i<=n; i++) p[i] = i;
-    } 
-    int find(int x) {return p[x] = x==p[x]? x: find(p[x]);}
+    vi p;
+    DSU(int ni) {p.resize(n+1, -1);} 
+    int find(int x) {return p[x] < 0? x: p[x] = find(p[x]);}
     void merge(int a, int b) {
         a = find(a), b = find(b);
         if (a==b) return;
-        if (rn[a] < rn[b]) swap(a, b);
+        if (p[a] > p[b]) swap(a, b);
+        p[a]+=p[b];
         p[b] = a;
-        rn[a]+=rn[b];
     }
+    int siz(int x) {return p[find(x)];}
 };
