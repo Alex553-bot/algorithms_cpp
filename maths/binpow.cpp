534e708e9001b2e37e8f025e68ab69df4f47b19e
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2026-06-09 19:38:17 -0400

    euler theorem

--- maths/binpow.cpp
+++ maths/binpow.cpp
@@ -6,3 +6,13 @@ int binpow(int base, int exp) {
 	if (exp&1) res = (res*base)%MOD;
 	return res;
 }
+// if exp is really huge and m is prime then a^k -> a^(k%mod)
+// Euler theorem: no restrictions neither(base,exp,mod): 
+int binpow(int base, int exp, int mod) {
+	int phi_m = get_totient(mod); // count how many number i less than m satisfies this cond: gcd(i,m)=1
+
+	if (exp > phi_m) {
+		k = phi_m + ( k % phi_m );
+	}
+	return binpow(base,exp);
+}
